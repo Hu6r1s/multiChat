b75a3809ef1bd94de75499e8829cb79ed81d1d7c
Hu6r1s <darmaa333@gmail.com>
2022-05-23 21:56:12 +0900

    test

--- server.py
+++ server.py
@@ -66,7 +66,7 @@ def information(client, addr):
         rooms[room] = [client]
 
 def main():
-    HOST = '172.17.147.80'
+    HOST = '김준환 등신'
     PORT = 12321
 
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
